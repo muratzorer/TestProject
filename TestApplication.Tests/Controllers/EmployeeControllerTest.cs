937b34e9c3c02df1871aeac1c7e5c557b4c5f97c
Murat Zorer <IS96705@gm.isbank.com.tr>
2016-08-02 15:22:13 +0300

    initial commit

---
+++ TestApplication.Tests/Controllers/EmployeeControllerTest.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+
+namespace TestApplication.Tests.Controllers
+{
+    [TestFixture]
+    public class EmployeeControllerTest
+    {
+        private const string IE_DRIVER_PATH = @"C:\SeleniumDriver";
+        private const string ScreenShotLocation = @"C:\ScreenShot";
+        [Test]
+        public void Get()
+        {
+            IWebDriver driver = null;
+            try
+            {
+                ChromeOptions o = new ChromeOptions();
+                o.AddArgument("disable-extensions");
+
+                driver = new ChromeDriver(IE_DRIVER_PATH, o);
+
+                driver.Navigate().GoToUrl("http://localhost:60006/");
+
+                // Find the text input element by its name
+                IWebElement enter = driver.FindElement(By.TagName("a"));
+
+                enter.Click();
+
+                TestEmployeeListScreen(driver, "Launch");
+
+                TestAddEmployee(driver);
+
+                TestEmployeeListScreen(driver, "Add");
+
+                TestEditEmployee(driver);
+
+                TestEmployeeListScreen(driver, "Edit");
+
+                TestDeleteEmployees(driver);
+
+                TestEmployeeListScreen(driver, "AfterDelete");
+            }
+            catch (Exception ex) { }
+            finally
+            {
+                driver.Quit();
+            }
+        }
+
+        private void TestDeleteEmployees(IWebDriver driver)
+        {
+            //Find the last employee
+            IWebElement checkBox = (IWebElement)((IJavaScriptExecutor)driver).ExecuteScript("return $('input:checkbox:last')[0]");
+            checkBox.Click();
+
+            //Find delete button and click
+            IWebElement delBtn = driver.FindElement(By.Id("Delete"));
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            Thread.Sleep(1000);
+            delBtn.Click(); //Perfome delete operation
+            Thread.Sleep(1000);
+
+        }
+
+        private void TestEditEmployee(IWebDriver driver)
+        {
+            //Find the element anchortag for the employee having name as 'Anderson'
+            IWebElement anchorTag = (IWebElement)((IJavaScriptExecutor)driver).ExecuteScript("return $('a',$(\"td:contains('Anderson')\").parent())[0]");
+            anchorTag.Click();
+
+            //Wait and then check until the control with id=Name is available
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until((d) => { return d.FindElement(By.Id("Name")); });
+
+            //Find all the lements
+            IWebElement name = driver.FindElement(By.Id("Name"));
+            IWebElement sal = driver.FindElement(By.Id("Sal"));
+            IWebElement submit = driver.FindElement(By.Id("Submit"));
+
+            //Set the data (Change the name)
+            name.SendKeys(" James");
+            submit.Click();
+
+            IAlert alert = null;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until((d) => { alert = d.SwitchTo().Alert(); return alert; });
+            alert.Accept();
+        }
+
+        private void TestAddEmployee(IWebDriver driver)
+        {
+            driver.FindElement(By.LinkText("Add")).Click();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until((d) => { return d.FindElement(By.Id("Name")); });
+
+            IWebElement name = driver.FindElement(By.Id("Name"));
+            IWebElement sal = driver.FindElement(By.Id("Sal"));
+            IWebElement submit = driver.FindElement(By.Id("Submit"));
+
+            name.SendKeys("Natasha");
+            sal.SendKeys("5000");
+            submit.Click();
+
+            IAlert alert = null;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until((d) => { alert = d.SwitchTo().Alert(); return alert; });
+            alert.Accept();
+        }
+
+
+        public void TestEmployeeListScreen(IWebDriver driver, string type)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until((d) => { return d.Title.ToLower().StartsWith("employeelist"); });
+            IWebElement addLink = driver.FindElement(By.LinkText("Add"));
+            IWebElement delLink = driver.FindElement(By.Id("Delete"));
+            Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
+            ss.SaveAsFile(ScreenShotLocation + "\\" + type + "EmpList.jpeg", System.Drawing.Imaging.ImageFormat.Jpeg);
+
+        }
+
+    }
+}
---
+++ TestApplication.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using TestApplication.Models;
+
+namespace TestApplication.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void FetchEmployeesTest()
+        {
+            List<Employee> empList = new List<Employee>();
+            empList.Add(new Employee() { EmpId = 1, Name = "Anderson", Salary = 2000 });
+            empList.Add(new Employee() { EmpId = 2, Name = "Johnson", Salary = 3000 });
+            empList.Add(new Employee() { EmpId = 3, Name = "Jackson", Salary = 4000 });
+
+            Assert.That(EmployeeStore.FetchEmployees()[0].EmpId, Is.EqualTo(empList[0].EmpId));
+            Assert.That(EmployeeStore.FetchEmployees()[0].Name, Is.EqualTo(empList[0].Name));
+            Assert.That(EmployeeStore.FetchEmployees()[0].Salary, Is.EqualTo(empList[0].Salary));
+        }
+    }
+}
